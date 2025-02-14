36e28aacf933bdfa6e41b72c98db2c1211e87f34
lizchu413 <lizchu413@gmail.com>
2025-02-14 17:31:47 -0500

    yo

--- minitorch/nn.py
+++ minitorch/nn.py
@@ -261,6 +261,7 @@ def softmax_loss(logits: Tensor, target: Tensor) -> Tensor:
     batch_size, num_classes = logits.shape
     targets = one_hot(target, num_classes=num_classes)
     log_sums = logsumexp(logits, dim=1)
+    print(f"targets shape: {targets.shape} log_sums shape: {log_sums.shape}")
     result = log_sums - targets
     ### END YOUR SOLUTION
     return result.view(batch_size, )
