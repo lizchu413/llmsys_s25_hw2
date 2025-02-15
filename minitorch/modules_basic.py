654d7600defb27afb32003137f24a43741e1ab31
lizchu413 <lizchu413@gmail.com>
2025-02-15 00:00:02 -0500

    yo

--- minitorch/modules_basic.py
+++ minitorch/modules_basic.py
@@ -76,6 +76,7 @@ class Dropout(Module):
         idxes = tensor_from_numpy(np.random.binomial(1, 1 - self.p_dropout, size=x.shape), backend=x.backend)
         print(f"sum of idxs: {idxes.sum()} shape: {x.shape}")
         res = x * idxes
+        print(f"x: {x}\nres: {res}")
         return res
         ### END YOUR SOLUTION
 
