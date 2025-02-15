d005525b280c6bf5a38282b395b69266326d92d4
lizchu413 <lizchu413@gmail.com>
2025-02-14 23:36:09 -0500

    yo

--- minitorch/modules_basic.py
+++ minitorch/modules_basic.py
@@ -120,9 +120,9 @@ class Linear(Module):
         ### BEGIN YOUR SOLUTION
         self.in_size = in_size
         weights = np.random.rand(in_size, out_size) * 2 / np.sqrt(in_size) - 1 / np.sqrt(in_size)
-        self.weights = Parameter(weights)
-        bias = np.random.rand(out_size, ) * 2 / np.sqrt(out_size) - 1 / np.sqrt(out_size)
-        self.bias = Parameter(bias)
+        self.weights = Parameter(Tensor(weights, backend=backend))
+        bias_vals =  np.random.rand(out_size, ) * 2 / np.sqrt(out_size) - 1 / np.sqrt(out_size) if bias else np.zeros(out_size).reshape(-1, )
+        self.bias = Parameter(Tensor(bias_vals, backend=backend))
         ### END YOUR SOLUTION
 
     def forward(self, x: Tensor):
